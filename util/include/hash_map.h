6754d18c4eb17db586b73808b67c37940ead6169
xhd0216 <xhd0216@gmail.com>
2015-10-03 23:51:41 -0700

    fix minor bugs

--- util/include/hash_map.h
+++ util/include/hash_map.h
@@ -8,7 +8,6 @@ typedef char * (*value_to_string_cb_f)(const void *);
 typedef struct hash_map_entry_s{
 	void * key;
 	void * value;
-	int key_size;
 	value_clone_cb_f * value_clone;
 	value_free_cb_f * value_free;
 	struct hash_map_entry_s * next;
@@ -19,6 +18,12 @@ typedef struct hash_map_entry_s{
 
 typedef int (*hash_map_function)(const void * key);
 typedef struct hash_map_s{
+	/* I put all key-related functions on the hash_map_t
+	 * Because I want the keys to have the same format
+	 * at least, the hash map should know how to compare two keys
+	 * it is hard to implement a cmp function of two keys in 
+	 * different format
+	 */
 	hash_map_function * hash_f;
 	key_cmp_cb_f * key_cmp;
 	key_clone_cb_f * key_clone;
@@ -26,24 +31,25 @@ typedef struct hash_map_s{
 	hash_map_entry_t * table[HASH_MAP_MAX_BUCKETS];
 }hash_map_t;
 
-int hash_map_delete(hash_map_t *,
-					const void *,
-					int);
+int hash_map_delete_entry(hash_map_t *,
+					const void *);
 /* be careful of the call back function:
  * could cbf leak memory? */
 /* clone value using call back function */
 int hash_map_lookup(hash_map_t *,
 					const void * key,
-					//int key_size,
 					void ** value);
-					//value_clone_cb_f);
 int hash_map_init(hash_map_t **,
 				hash_map_function *,
 				key_cmp_cb_f *,
-				key_free_cb_f *);
+				key_free_cb_f *,
+				key_clone_cb_f *);
 /* copy value using call back function */
 int hash_map_insert(hash_map_t *,
 					const void * key,
 					const void * value,
-					value_clone_cb_f,
-					value_free_cb_f);
+					value_clone_cb_f *,
+					value_free_cb_f *);
+void hash_map_dump(hash_map_t *,
+				key_to_string_cb_f *,
+				value_to_string_cb_f *);
--- util/src/hash_map.c
+++ util/src/hash_map.c
@@ -41,7 +41,8 @@ void hash_map_free_entry(hash_map_entry_t * p, key_free_cb_f * key_f){
 int hash_map_init(hash_map_t ** hm,
 				hash_map_function * f,
 				key_cmp_cb_f * key_c,
-				key_free_cb_f * key_f){
+				key_free_cb_f * key_f,
+				key_clone_cb_f * key_cl){
 	if(!hm) return 0;
 	*hm = malloc(sizeof(hash_map_t));
 	if(!*hm) return 0;
@@ -59,16 +60,12 @@ int hash_map_init(hash_map_t ** hm,
 	(*hm)->hash_f = f;
 	(*hm)->key_cmp = key_c;
 	(*hm)->key_free = key_f;
+	(*hm)->key_clone = key_cl;
 	return 1;
 }
 int hash_map_bucket_number_valid(int b){
 	return (b >=0 && b < HASH_MAP_MAX_BUCKETS)?1:0;
 }
-int hash_map_key_compare(const void * key_1, int s1,
-						 const void * key_2, int s2){
-	if(!key_1 || !key_2 || s1 != s2) return 0;
-	return (memcmp(key_1, key_2, s1) == 0)?1:0;
-} 
 
 hash_map_entry_t * 
 hash_map_lookup_entry(hash_map_t * hm,
@@ -77,11 +74,16 @@ hash_map_lookup_entry(hash_map_t * hm,
 		return 0;
 	}
 	int buc = (*(hm->hash_f))(key);
-	if(hash_map_bucket_number_valid(buc)){
+	if(!hash_map_bucket_number_valid(buc)){
 		printf("%s: invalid bucket number %d\n", __FUNCTION__, buc);
 		return 0;
 	}
-	hash_map_entry_t * p = hm->table[buc]->next;
+	hash_map_entry_t * p = hm->table[buc];
+	if(!p){
+		printf("%s: hashmap->table[%d] is NULL\n", __FUNCTION__, buc);
+		return 0;
+	}
+	p = p->next;
 	while(p){
 		if((*(hm->key_cmp))(key, p->key)){
 			break;
@@ -112,7 +114,7 @@ int hash_map_delete_entry(hash_map_t * hm,
 	hash_map_entry_t * p = hash_map_lookup_entry(hm, key);
 	if(!p){
 		printf("%s: cannot find entry\n", __FUNCTION__);
-		return -1;
+		return 0;
 	}
 	p->prev->next = p->next;
 	if(p->next) p->next->prev = p->prev;
@@ -130,45 +132,45 @@ int
 hash_map_insert(hash_map_t * hashmap,
 				const void * key,
 				const void * value,
-				value_clone_cb_f clone,
-				value_free_cb_f value_f){
-	if(!hashmap || !key || !value || !clone || !(hashmap->key_cmp) || !(hashmap->key_cmp)){
+				value_clone_cb_f * clone,
+				value_free_cb_f * value_f){
+	if(!hashmap || !key || !value || !clone || !(hashmap->key_cmp) || !value_f){
 		return 0;
 	}
+	void * new_value = 0;
 	int res = 0;
+	int is_new = 0;
 	hash_map_entry_t * new_node;
-	/* if the key already exists, update it */
+	res = (*clone)(&(new_node->value), value);
+	if(!(new_node->value)){
+		printf("%s: error cloning memory for value\n", __FUNCTION__);
+		return 0;
+	}
 	new_node = hash_map_lookup_entry(hashmap, key);
 	if(new_node){
 		printf("%s: entry already exists, update it\n", __FUNCTION__);
-		void * new_value;
-		res = (*clone)(&new_value, value);	
-		if(!new_value || res < 1){
-			printf("%s: error cloning memory for value, stop update\n", __FUNCTION__);
-			return 0;
-		}
+		is_new = 1;
+		/* if the key already exists, discard old value */
 		(*(new_node->value_free))(new_node->value);
-		new_node->value = new_value;
+		/* we keep the old key here. Assuming transitivity of key_cmp_cb_f*/
 	}else{
+		printf("%s: inserting new entry\n", __FUNCTION__);
 		new_node = (hash_map_entry_t *) malloc(sizeof(hash_map_entry_t));
 		if(!new_node){
 			printf("%s: error allocating memory\n", __FUNCTION__);
 			return 0;
 		}
-		res = (*clone)(&(new_node->value), value);
-		if(!(new_node->value)){
-			printf("%s: error cloning memory for value\n", __FUNCTION__);
-			free(new_node);
-			return 0;
-		}
 		/*clone key*/
 		(*(hashmap->key_clone))(&(new_node->key), key);
 	}
-	new_node->value_clone = &clone;
-	new_node->value_free = &value_f;
+	new_node->value = new_value;
+	new_node->value_clone = clone;
+	new_node->value_free = value_f;
+	if(is_new) return 1;
 	int buc = (*(hashmap->hash_f))(key);
 	if(!hash_map_bucket_number_valid(buc)){
 		printf("%s: bucket number not valid: %d\n", __FUNCTION__, buc);
+		hash_map_free_entry(new_node, hashmap->key_free);
 		return 0;
 	}
 	hash_map_entry_t * p = hashmap->table[buc];

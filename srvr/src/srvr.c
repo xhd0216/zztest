0bec55e125d410a19f609ef5d525d56228a6ea51
xhd0216 <xhd0216@gmail.com>
2015-10-25 20:49:04 -0700

    not yet finished

--- srvr/src/srvr.c
+++ srvr/src/srvr.c
@@ -3,17 +3,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+
+/*shared memory*/
+lru_cache_t * lru = NULL;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 void * thread_main(void * arg){
 	printf("thread %s is running\n", __FUNCTION__);
+	while(1){
+		break;	
 
+	}
 	printf("thread %s quits\n", __FUNCTION__);
 	return 0;
 }
 
 int main(int argc, char * argv[]){
 	pthread_t t1;
+	int res = 0;
 	if(argc == 2){
 		if(!strcmp(argv[1], "start")){
+			pthread_mutex_lock(&mutex);
+			res = lru_cache_init_wrap(&lru);
+			pthread_mutex_unlock(&mutex);
+			if(!lru || !res){
+				printf("%s: failed to initialize cache, quit program\n", __FUNCTION__);
+				return 0;
+			}
 			pthread_create(&t1, NULL, &thread_main, NULL);
 			pthread_join(t1, NULL);
 			printf("thread joined\n");

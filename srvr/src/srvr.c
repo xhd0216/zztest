76de68f96b46864cb9d2c13217c8e32ab2b2ce14
xhd0216 <xhd0216@gmail.com>
2015-10-26 23:56:21 -0700

    srvr unix socket not tested

--- srvr/src/srvr.c
+++ srvr/src/srvr.c
@@ -3,17 +3,68 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define SERVER_PATH_NAME "_server_file_"
+#define MSG_LENGTH 1024
 
 /*shared memory*/
 lru_cache_t * lru = NULL;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void * thread_main(void * arg){
-	printf("thread %s is running\n", __FUNCTION__);
-	while(1){
-		break;	
-
+	int sock, msgsock, rval;
+	struct sockaddr_un server;
+	char buf[MSG_LENGTH];
+	int b_end = 0;
+	sock = socket(AF_UNIX, SOCK_STREAM, 0);
+	if(sock < 0){
+		printf("%s: opening stream socket error: %d\n", 
+				__FUNCTION__, sock);
+		return 0;
 	}
+	server.sun_family = AF_UNIX;
+	strcpy(server.sun_path, SERVER_PATH_NAME);
+	if(bind(sock, (struct sockaddr *)&server, sizeof(struct sockaddr_un))){
+		printf("%s: error in binding stream socket\n", __FUNCTION__);
+		return 0;
+	}
+	printf("thread %s is running, socket has name %s\n", __FUNCTION__, server.sun_path);
+	/* listen, max 10 connections waiting*/
+	listen(sock, 10);
+	while(1){
+		printf("waiting for connection...\n");
+		msgsock = accept(sock, 0, 0);
+		if(msgsock == -1){
+			printf("received msg error.\n");
+		}
+		else{
+			rval = 1;
+			while(rval > 0){
+				bzero(buf, sizeof(buf));
+				rval = read(msgsock, buf, MSG_LENGTH);
+				if(rval<0){
+					printf("msg read error\n");
+				}
+				else if(rval == 0){
+					printf("ending connection\n");
+				}
+				else{
+					printf("-->%s\n", buf);
+					if(strncmp(buf, "END", 3) == 0){
+						b_end = 1;
+					}
+				}
+			}
+		}
+		close(msgsock);
+		if(b_end) break;
+	}
+	close(sock);
+	/*unlink file, otherwise, other socket cannot connect*/
+	unlink(SERVER_PATH_NAME);
 	printf("thread %s quits\n", __FUNCTION__);
 	return 0;
 }

ff6eb4f3a2a62a746d4aad5e9c431e14bab805be
xhd0216 <xhd0216@gmail.com>
2015-11-08 19:13:45 -0800

    add a python test client

---
+++ srvr/test.py
@@ -0,0 +1,17 @@
+import socket
+import sys
+
+# Create a UDS socket
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = './server_file/_server_file_'
+print >>sys.stderr, 'connecting to %s' % server_address
+try:
+    sock.connect(server_address)
+except socket.error, msg:
+    print >>sys.stderr, msg
+    sys.exit(1)
+sock.sendall("ping")
+data = sock.recv(1024)
+print data

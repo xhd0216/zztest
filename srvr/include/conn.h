219f09d3c296719d64253f1948eb54e25a4ae6f5
xhd0216 <xhd0216@gmail.com>
2015-11-11 23:56:16 -0800

    adding multi-thread

---
+++ srvr/include/conn.h
@@ -0,0 +1,6 @@
+
+typedef struct conn_args_s{
+	int msgsock; /*socket*/
+}conn_args_t;
+
+void * thread_worker(void *);
--- srvr/src/srvr.c
+++ srvr/src/srvr.c
@@ -1,4 +1,3 @@
-//#include "lru_cache.h"
 #include "data.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,8 +14,9 @@
 
 #define SERVER_PATH_NAME "./server_file/_server_file_"
 #define MSG_LENGTH 1024
+#define MX_LISTEN 10
+#define NUM_OF_WORKING_THREAD 4
 
-int b_end = 0;
 
 #define SIGNAL_T SIGINT
 void sig_handler(int sig){
@@ -50,6 +50,7 @@ int send_all(int socket, const void *buffer, size_t length)
     }
     return 1;
 }
+#if 0
 void * thread_main(void * arg){
 	int sock, msgsock, rval;
 	struct sockaddr_un server;
@@ -113,11 +114,16 @@ void * thread_main(void * arg){
 	printf("thread %s quits\n", __FUNCTION__);
 	return 0;
 }
-
+#endif
 int main(int argc, char * argv[]){
 	pthread_t t1;
 	int res = 0;
 	struct sigaction act;
+	int b_end = 0;
+	int sock, msgsock, rval;
+	struct sockaddr_un server;
+	char buf[MSG_LENGTH];
+	
 	if(argc != 2){
 		printf("invalid num. of arguments\n");
 		return 0;
@@ -130,16 +136,71 @@ int main(int argc, char * argv[]){
 		act.sa_handler = sig_handler;
 		sigaction(SIGNAL_T, &act, 0);
 
+		/*initialize share memory*/
 		pthread_mutex_lock(&mutex);
 		res = lru_cache_init_wrap(&lru);
 		pthread_mutex_unlock(&mutex);
+	
 		if (!lru || !res){
 			printf("%s: failed to initialize cache, quit program\n", __FUNCTION__);
 			return 0;
 		}
-		pthread_create(&t1, NULL, &thread_main, NULL);
-		pthread_join(t1, NULL);
-		printf("thread joined\n");
+		/*create socket*/
+		sock = socket(AF_UNIX, SOCK_STREAM, 0);
+		if(sock < 0){
+			printf("%s: opening stream socket error: %d\n", 
+					__FUNCTION__, sock);
+			return 0;
+		}
+		server.sun_family = AF_UNIX;
+		strcpy(server.sun_path, SERVER_PATH_NAME);
+		if(bind(sock, (struct sockaddr *)&server, sizeof(struct sockaddr_un))){
+			printf("%s: error in binding stream socket\n", __FUNCTION__);
+			return 0;
+		}
+		chmod(SERVER_PATH_NAME, S_IRWXU|S_IRWXG|S_IROTH|S_IWOTH);
+		
+		printf("program is running, socket name: %s\n", server.sun_path);
+		/* listen, max 10 connections waiting*/
+		listen(sock, MX_LISTEN);
+		while(1){
+			printf("waiting for connection...\n");
+			msgsock = accept(sock, 0, 0);
+			if (msgsock == -1){
+				printf("received msg error.\n");
+			} else {
+				rval = 1;
+				while(rval > 0){
+					bzero(buf, sizeof(buf));
+					rval = read(msgsock, buf, MSG_LENGTH);
+					if (rval<0){
+						printf("msg read error\n");
+					} else if (rval == 0){
+						printf("ending connection\n");
+					} else {
+						printf("msg read-->%s\n", buf);
+						if(strcmp(buf, "END") == 0){
+							b_end = 1;
+							break;
+						}
+						const char * resp_buf = "got some msg\n";
+						int ret = send_all(msgsock, (const void *)resp_buf, strlen(resp_buf));
+						if (ret) {
+							printf("msg sent\n");
+						} else {
+							printf("failed to send msg\n");
+						}
+					}
+					rval = -1;
+				}
+				close(msgsock);
+			}
+			if(b_end) break;
+		}
+		close(sock);
+		/*unlink file, otherwise, other socket cannot connect*/
+		unlink(SERVER_PATH_NAME);
+		printf("thread %s quits\n", __FUNCTION__);
 	}		
 	return 0;
 

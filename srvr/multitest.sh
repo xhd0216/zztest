78f5772a03aeb0673b1fc2b3cc6c870b93239454
xhd0216 <xhd0216@gmail.com>
2015-12-04 00:25:06 -0800

    new test

--- srvr/multitest.sh
+++ srvr/multitest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for(( c=0; c<10; c++ ))
+for(( c=0; c<100; c++ ))
 do
-	python test.py
+	curl http://192.168.0.14/stest.php
+	#sleep 1
 done
---
+++ srvr/wrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for (( c=0; c<10; c++ ))
+do
+	./multitest.sh &
+done

c01a0196a35b14c06f5c4a5f0621a4571a924db7
xhd0216 <xhd0216@gmail.com>
2016-04-25 00:26:08 -0700

    fl allocate

--- memory/include/fl_alloc.h
+++ memory/include/fl_alloc.h
@@ -26,24 +26,27 @@
 #define ALLOC_OK                     1
 
 struct free_list_s{
-	struct free_list_s * prev;
-	struct free_list_s * next;
-	void * data;
+	//struct free_list_s * prev;
+	//struct free_list_s * next;
+	void * next;
+	//void * data;
 };
 typedef struct free_list_s free_list_t;
 
 typedef struct free_list_allocator_s{
 	int block_size; /* size of each block, MUST be a power of 2 */
 	int block_num;  /* number of blocks in the region */
+	int actual_size; /* the actual available size of the block, equals block_size - sizeof(free_list_t) */
 	void * begin;   /* beginning of the whole block */
 	void * end;     /* end of the whole block */
-	free_list_t * head; /* head of list of available memory block */
+	free_list_t head; /* head of list of available memory block */
 	int num_avail;  /* number of available blocks */
 }fl_allocator_t;
 
 typedef struct fl_allocator_param_s{
 	int size;
 	int num;
+	allocator_t * alloc;
 }fl_allocator_init_param_t;
 
 fl_allocator_t * fl_allocator_construct(fl_allocator_init_param_t *);
--- memory/src/fl_alloc.c
+++ memory/src/fl_alloc.c
@@ -1,5 +1,7 @@
 #include "fl_alloc.h"
 
+
+/* will check if b is aligned */
 int chunk_is_in_range(fl_allocator_t * alloc, void * b){
 	if (b >= alloc->begin && b < alloc->end){
 		if ((b - alloc->begin) & alloc->block_size){
@@ -14,6 +16,11 @@ int chunk_is_in_range(fl_allocator_t * alloc, void * b){
 /* TODO: consider using user-defined allocator rather than malloc, need to indicate in param */
 fl_allocator_t * fl_allocator_construct(fl_allocator_init_param_t * param)
 {
+	if (param->size <= (sizeof(free_list_t))) {
+		/* requested size is too small, not efficient */
+		printf("%s: block size too small\n", __func__);
+		return NULL;
+	}
 	/* validate the parameters */
 	if ((param->size ^ (param->size-1)) != 0){
 		/* block size is NOT a power of 2 */
@@ -33,6 +40,7 @@ fl_allocator_t * fl_allocator_construct(fl_allocator_init_param_t * param)
 		return r;
 	}
 	r->block_size = param->size;
+	r->actual_size = param->size - sizeof(free_list_t);
 	r->block_num = param->num;
 	if (param->alloc == NULL){
 		r->begin = (void *)malloc(param->size * param->num);
@@ -48,6 +56,26 @@ fl_allocator_t * fl_allocator_construct(fl_allocator_init_param_t * param)
 	r->end = r->begin + param->size * param->num;
 	r->num_avail = param->num;
 	/* build the linked list of available chunks */
-	
+	void * i = r->begin;
+	void * j = r->begin + r->block_size;
+	while(i < r->end){
+		((free_list_t *) i)->next = j;
+		i = j;
+		j = j + sizeof(free_list_t);
+	}
+	(free_list_t *)(i - sizeof(free_list_t))->next = NULL;
+	r->head.next = (free_list_t) r->begin;
+	return r;
+}
 
+void * fl_alloc(fl_allocator_t * fl){
+	/* TODO: add locks */
+	if (fl->num_avail == 0 || fl->head.next == NULL){
+		printf("%s: fixed list allocator (size %d) is empty\n", __func__, fl->actual_size);
+		return NULL;
+	}
+	free_list_t * tmp = (free_list_t *) fl->head.next;
+	fl->head.next = tmp->next;
+	tmp->next = NULL;
+	return (void *) tmp + sizeof(free_list_t);
 }

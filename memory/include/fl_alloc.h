444d7a94636e56da8fdd8e1df1145a9509a18521
xhd0216 <xhd0216@gmail.com>
2016-04-29 00:10:30 -0700

    fixed size allocator

--- memory/include/fl_alloc.h
+++ memory/include/fl_alloc.h
@@ -25,6 +25,7 @@
 #define ALLOC_ERR_ADDR_OUT_OF_RANGE -3
 #define ALLOC_OK                     1
 
+
 struct free_list_s{
 	//struct free_list_s * prev;
 	//struct free_list_s * next;
@@ -33,23 +34,49 @@ struct free_list_s{
 };
 typedef struct free_list_s free_list_t;
 
+
+/* extra allocating function that when fixed list allocator is full,
+   it is called to allocate memory, like malloc.
+   parameter: 
+		alloc: allocator
+		size:  size to allocate
+   return value:
+		a pointer to memory;
+		size of the memory stored in size
+*/
+typedef (void *)(*extra_allocator_f)(void * alloc, int * size);
+typedef (void) (*extra_free_f)(void * alloc, void * v);
+
 typedef struct free_list_allocator_s{
 	int block_size; /* size of each block, MUST be a power of 2 */
 	int block_num;  /* number of blocks in the region */
-	int actual_size; /* the actual available size of the block, equals block_size - sizeof(free_list_t) */
 	void * begin;   /* beginning of the whole block */
 	void * end;     /* end of the whole block */
 	free_list_t head; /* head of list of available memory block */
 	int num_avail;  /* number of available blocks */
+	int min_size;   /* minimal size to allocate, 1 by default */
+	int extra_size; /* total size that allocated by extra allocator */
+	int extra_size_max; /* maximum size that allowed for extra allocate */
+	void * alloc;   /* allocator to extra memory */
+	/* note: we don't free alloc when we destruct fl_allocator_t */
+	extra_allocator_f extra_malloc; /* function to allocate memory when fl is full */
+	extra_free_f      extra_free;
+	/* extra allocator could be NULL -- in that case, when fl allocator is full,
+	   return NULL, then let the caller decide what to do. That is useful when an
+	   allocator which consists of multiple fl allocators and one extra allocator */
 }fl_allocator_t;
 
 typedef struct fl_allocator_param_s{
 	int size;
 	int num;
-	allocator_t * alloc;
+	int min_size;
+	void * alloc; /* extra allocator */
+	extra_allocator_f eaf;
+	extra_free_f eff;
+	int extra_size_max;
 }fl_allocator_init_param_t;
 
 fl_allocator_t * fl_allocator_construct(fl_allocator_init_param_t *);
 void fl_allocator_destruct(fl_allocator_t *);
-void * fl_alloc(fl_allocator_t *);
-void fl_free(void *);
+void * fl_alloc(fl_allocator_t *, int);
+void fl_free(fl_allocator_t *, void *);
--- memory/src/fl_alloc.c
+++ memory/src/fl_alloc.c
@@ -1,8 +1,30 @@
+/*************************************
+ *
+ * copyright: xhd0216@gmail.com
+ *
+ ************************************/
+
+
 #include "fl_alloc.h"
 
 
+
+/* extra allocation function, input allocator could be NULL */
+void * extra_alloc_func(void * allocator, int * size)
+{
+	void * ret = (void *)malloc(*size);
+	return ret;
+	/* error is handled outside this function */
+}
+void extra_free_func(void * allocator, void * v)
+{
+	free(v);
+}
+
 /* will check if b is aligned */
 int chunk_is_in_range(fl_allocator_t * alloc, void * b){
+	printf("%s: validating address, begin=%p, end=%p, addr=%p, size=%d\n",
+			alloc->begin, alloc->end, b, alloc->block_size);
 	if (b >= alloc->begin && b < alloc->end){
 		if ((b - alloc->begin) & alloc->block_size){
 			return ALLOC_ERR_ADDR_ALIGN_ERR;
@@ -14,9 +36,10 @@ int chunk_is_in_range(fl_allocator_t * alloc, void * b){
 }
 
 /* TODO: consider using user-defined allocator rather than malloc, need to indicate in param */
-fl_allocator_t * fl_allocator_construct(fl_allocator_init_param_t * param)
+fl_allocator_t * 
+fl_allocator_construct(fl_allocator_init_param_t * param)
 {
-	if (param->size <= (sizeof(free_list_t))) {
+	if (param->size < (sizeof(free_list_t))) {
 		/* requested size is too small, not efficient */
 		printf("%s: block size too small\n", __func__);
 		return NULL;
@@ -39,15 +62,13 @@ fl_allocator_t * fl_allocator_construct(fl_allocator_init_param_t * param)
 		printf("%s: cannot allocate memory for allocator\n", __func__);
 		return r;
 	}
-	r->block_size = param->size;
-	r->actual_size = param->size - sizeof(free_list_t);
-	r->block_num = param->num;
-	if (param->alloc == NULL){
+	//if (param->alloc == NULL){
+	//	r->begin = (void *)malloc(param->size * param->num);
+	//} else {
+		/* TODO: consider using user-defined allocator? 
+		   and, don't mess up with extra allocator	*/
 		r->begin = (void *)malloc(param->size * param->num);
-	} else {
-		/* TODO: consider using user-defined allocator? */
-		r->begin = (void *)malloc(param->size * param->num);
-	}
+	//}
 	if (r->begin = NULL) {
 		printf("%s: cannot allocate memory of size %d\n", __func__, param->size * param->num);
 		free(r);
@@ -55,27 +76,79 @@ fl_allocator_t * fl_allocator_construct(fl_allocator_init_param_t * param)
 	}
 	r->end = r->begin + param->size * param->num;
 	r->num_avail = param->num;
+	r->block_size = param->size;
+	r->min_size = param->min_size > 1? param->min_size:1;
+	r->block_num = param->num;
+	r->extra_size = 0;
+	r->extra_size_max = param->extra_size_max;
+	r->alloc = param->alloc;
+	r->extra_malloc = param->eaf;
+	r->extra_free = param->eff;
 	/* build the linked list of available chunks */
 	void * i = r->begin;
 	void * j = r->begin + r->block_size;
-	while(i < r->end){
+	while(j < r->end){
 		((free_list_t *) i)->next = j;
 		i = j;
 		j = j + sizeof(free_list_t);
 	}
-	(free_list_t *)(i - sizeof(free_list_t))->next = NULL;
+	(free_list_t *)i->next = NULL;
 	r->head.next = (free_list_t) r->begin;
 	return r;
 }
+void fl_allocator_destrcut(fl_allocator_t * fl)
+{
+	if (!fl) return;
+	if (fl->begin) free(fl->begin); /* TODO: what if we did not use malloc to allocate fl->begin ? */
+	free(fl);
+}
 
-void * fl_alloc(fl_allocator_t * fl){
+
+void * fl_alloc(fl_allocator_t * fl, int size){
 	/* TODO: add locks */
+	if (size > fl->block_size || size < fl->min_size) {
+		printf("%s: requested size is %d, does not fit in a block\n", __func__, size);
+		return NULL;
+	}
+
 	if (fl->num_avail == 0 || fl->head.next == NULL){
-		printf("%s: fixed list allocator (size %d) is empty\n", __func__, fl->actual_size);
+		printf("%s: fixed list allocator (size %d) is empty\n", __func__, fl->block_size);
+		if(fl->extra_malloc && fl->extra_size + size <= fl->extra_size_max){
+			int s = size;
+			void * ret = fl->extra_malloc(fl->alloc, &s);
+			if (ret != NULL) {
+				fl->extra_size += s;
+				return ret;
+			}
+		}
 		return NULL;
 	}
 	free_list_t * tmp = (free_list_t *) fl->head.next;
 	fl->head.next = tmp->next;
 	tmp->next = NULL;
-	return (void *) tmp + sizeof(free_list_t);
+	memset(tmp, 0, fl->block_size);
+	fl->num_avail -= 1;
+
+	return (void *) tmp;
+}
+
+void fl_free(fl_allocator_t * fl, void * v)
+{
+	if (!fl || !fl->begin) {
+		return;
+	}
+	int ret = chunk_is_in_range(fl, v);
+	if (ret == ALLOC_ERR_ADDR_OUT_OF_RANGE) {
+		/* in extra allocator */
+		if (fl->extra_free) {
+			fl->extra_free(fl->alloc, v);
+		}
+		return;
+	} else if (ret != ALLOC_OK) {
+		/* error */
+		return;
+	}
+	free_list_t * t = (free_list_t *) v;
+	t->next = fl->head.next;
+	t = fl->head.next = t;
 }
